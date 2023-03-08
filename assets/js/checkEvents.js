b364570113a9de4d64c4244a52b4e454691e786d
Lean <lea.ezequiel@live.com>
2023-03-08 15:13:29 -0300

    all the categories are made usuing .map

--- assets/js/checkEvents.js
+++ assets/js/checkEvents.js
@@ -14,6 +14,13 @@ function printChecks(id_etiqueta,events) {
 }
 printChecks('#table_checks',category)
 
+
+//ESTE ERA MI BOTON. NO SE BORRA POR LAS DUDAS
+  <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Type anything" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+
 */
 
 //aca ya guarde las categorias 
@@ -30,10 +37,14 @@ function printChecks(id_etiqueta,eventsCategories) {
     let container = document.querySelector(id_etiqueta)
     eventsCategories = eventsCategories.map(each=> {
         return `
-        <fieldset>
-            <label class="contact-label" for="${each}">${each}</label>
-            <input onclick="captureData()" class="class_checks contact-input" type="checkbox" value="${each}" name="category" id="${each}">
-        </fieldset>
+
+        <li class="nav-item btn btn-secondary">
+        <div class="form-check">
+          <input onclick="captureData()" class="class_checks contact-input" type="checkbox" value="${each}" name="category" id="${each}">
+          <label class="contact-label" for="${each}">${each}</label>
+        </div>
+        </li>
+
         `
     })
     eventsCategories.push(`<input onkeyup="captureData()" id="id_search" class="contact-input" type="text" name="texto" placeholder="search">`)
